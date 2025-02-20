4a742be9639bc5e30e3a564947c5ca8aaccfbcea
rustamali-786 <rustamer786@gmail.com>
2025-02-21 03:30:24 +0530

    Add files via upload

---
+++ iovo.java
@@ -0,0 +1,73 @@
+
+// import java.util.Scanner;
+//public class aahil {
+//    public static void main(String[]args){
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Eter Radius here ");
+//        double radius = sc.nextDouble();
+//        double pi = 3.1415;
+//        double area = pi * radius * radius;
+//        System.out.println("The area of circle is "+area);
+//    }
+//}
+//
+//import java.util.Scanner;
+//public class aahil{
+//    public static void main(String[]args){
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter First Number ");
+//        int x = sc.nextInt();
+//        System.out.print("Enter Second Number ");
+//        int y = sc.nextInt();
+//     int sum = x + y;
+//        System.out.println("The Addiition is "+sum);
+//    }
+//}
+//public class aahil {
+//    public static void main(String[] args) {
+//        int x = 10, y = 2;
+//        System.out.println(y+x);
+//        System.out.println(x-y);
+//        System.out.println(x*y);
+//        System.out.println(x/y);
+//        System.out.println(x%y);
+//    }
+//}
+//public class aahil{
+//    public static void main(String[] args) {
+//        int x = 6,y = 9;
+//        System.out.println(x ==y); //false
+//       System.out.println(x!=y); // true
+//        System.out.println(x>y); // false
+//        System.out.println(x>=y); //false
+//        System.out.println(x<y); //true
+//        System.out.println(x<=y); // true
+//
+//    }
+//}
+//
+//public class aahil {
+//    public static void main(String[]args){
+//        int x = 15,y = 10,z = 5;
+//
+//        //  LOGICAL AND GATE(&&) AND GATE KA KAM HOTA HA KI AGAR DONO TRUE HA TAB TRUE DEGA
+//        //OUTPUT ME AGAR USME SE KOI AGAR FALSE HOGA TO OUTPUT FALSE HOGA
+//
+//        System.out.println((x > y) && (y>z)); // true
+//        System.out.println((x<y) && (y>z)); // false
+//
+//
+//        // LOGICAL OR GATE (//) OR GATE KA KAM HOTA HA KI AGAR DONO ME SE KOI EAK AGAR SAHI
+//        // HA TO OUTPUT TRUE DEGA
+//
+//        System.out.println((x < y) || (y>z) ); //true
+//        System.out.println((x>y) || (y<z)); // true
+//        System.out.println((x==y) || (y<z)); // false
+//
+//        // NOT(!) SYMBOL NOT GATE OUTPUT KP REVERSE KAR DETA HA YE SAHI LO GALAT KAR DETA HA
+//        // AUR GALAT KO SAHI KAR DETA HA
+//
+//        System.out.println(!(x == y)); //true
+//        System.out.println(!(x>y)); //false
+//        System.out.println(!(x<y)); //true
+ 
