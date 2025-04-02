d75f6e59148abac19f82de63c530d5e67f0e523b
rustamali-786 <rustamer786@gmail.com>
2025-04-03 01:29:36 +0530

    Add files via upload

---
+++ pattern_printing.java
@@ -0,0 +1,165 @@
+          // Square/Rectangles
+
+// Q 1: print star   *****
+ //                  *****
+//                   *****
+
+//public class pattern_printing{
+//    public static void main(String[] args) {
+//        for(int i = 1; i<=3;i++){
+//            System.out.println("*****");
+//        }
+//    }
+//}
+
+// Q 2. print star input from user.
+
+//import java.util.Scanner;
+//public class pattern_printing{
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println(" Enter number of rows : ");
+//        int m = sc.nextInt();
+//       System.out.println(" enter a number of colums : ");
+//       int n = sc.nextInt();
+//        for(int i = 1; i<=m;i++){
+//            for(int j = 1; j<=n;j++){
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+//
+//    }
+//}
+
+// Q:3. print the given pattern.
+//       1234
+//       1234
+//       1234
+//       1234
+
+//public class pattern_printing{
+//    public static void main(String[] args) {
+//        for(int i = 1; i<=4;i++){
+//            System.out.println("1234");
+//        }
+//    }
+//}
+
+//   agar yahi user jab 1234... tak print karne ke liye bole to aur kitna lines (rows) lets do this.
+
+//import java.util.Scanner;
+//public class pattern_printing{
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print(" Enter a rows : ");
+//        int m = sc.nextInt();
+//        System.out.print(" enter a number : ");
+//        int n = sc.nextInt();
+//        for(int i = 1;i <=m;i++){
+//            for(int j = 1; j<=n;j++){
+//                System.out.print(j+" ");
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
+
+// Q:4. print this.
+//     1 1 1 1
+//     2 2 2 2
+//     3 3 3 3
+//     4 4 4 4
+
+//public class pattern_printing{
+//    public static void main(String[] args) {
+//        for(int i = 1;i<=4;i++){
+//            for(int j = 1; j<=4;j++){
+//                System.out.print(i);
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
+
+// agar yahi user input liya to
+
+//import java.util.Scanner;
+//public class pattern_printing{
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print(" enter a rows : ");
+//        int m = sc.nextInt();
+//        System.out.print(" enter a colums : ");
+//        int n = sc.nextInt();
+//        for(int i = 1; i<=m;i++){
+//            for(int j = 1; j<=n;j++){
+//                System.out.print(i+" ");
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
+
+// Q:5. print the given pattern.
+//  A B C D
+//  A B C D
+//  A B C D
+//  A B C D
+//public class pattern_printing{
+//    public static void main(String[] args) {
+//        for(int i = 1; i<=4; i++){
+//            for(int j = 65; j<=68; j++ ){
+//                System.out.print((char)j + " ");
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
+
+// user se input lets do this.
+
+//import java.util.Scanner;
+//public class pattern_printing{
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println(" enter a rows : ");
+//        int m = sc.nextInt();
+//        System.out.print(" e nter a number from 65 to 90 : ");
+//        int n = sc.nextInt();
+//        for(int i = 1; i<=m; i++){
+//            for(int j = 65; j<=n; j++){
+//                System.out.print((char)j + " ");
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
+
+
+                 // Star tringle
+
+// Q:1.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
