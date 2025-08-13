eb7a9f581b83be6dae2fc04b2463b68e4c4fe675
rustamali-786 <rustamer786@gmail.com>
2025-08-14 02:45:18 +0530

    Add files via upload

--- TwoDArray.java
+++ TwoDArray.java
@@ -523,9 +523,163 @@ public class TwoDArray {
 */
 
 
+/*
+Q. precalculating the horizontal sum each row in the matrix. method 2
+
+ */
+
+/*
+
+import java.util.Scanner;
+public class TwoDArray {
+    static int findsum(int [][] matrix , int l1, int r1, int l2, int r2){
+        int sum = 0;
+        for(int i = l1; i <= l2; i++){
+            for(int j = r1; j <= r2 ; j++){
+                sum += matrix[i][j];
+            }
+        }
+        return sum;
+    }
+    static void findprefixsummatrix(int [][] matrix) {
+        int r = matrix.length;
+        int c = matrix[0].length;
+        // trverse horizontally to calculate ro wise prefix sum.
+        for (int i = 0; i < r; i++) {
+            for (int j = 1; j < c; j++) {
+                matrix[i][j] = matrix[i][j] + matrix[i][j - 1];
+            }
+        }
+    }
+    public static int findsum2(int [][] matrix , int l1, int r1, int l2, int r2){
+        int sum = 0;
+        findprefixsummatrix(matrix);
+        for(int i = l1; i <= l2; i++) {
+            if (r1 >= 1) {
+                sum += matrix[i][r2] - matrix[i][r1 - 1];
+            }
+            else{
+                sum += matrix[i][r2];
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter np of rows and col : ");
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+        int [][] matrix = new int [r][c];
+        System.out.println("Enter" +r*c +" elements : ");
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("Enter recangle boundries l1, r1, l2, r2 ");
+        int l1 = sc.nextInt();
+        int r1 = sc.nextInt();
+        int l2 = sc.nextInt();
+        int r2 = sc.nextInt();
+        System.out.println("Rectangle sum method 1 => " + findsum(matrix,l1,r1,l1,r2));
+        System.out.println("Rectangle sum of method 2 => " + findsum2(matrix,l1,r1,l1,r2));
+
+    }
+}
+
+ */
 
 
 
+/*
+Q. precalculating the horizontal sum each row in the matrix. method 3
+ */
+/*
+import java.util.Scanner;
+public class TwoDArray {
+    static int findsum(int [][] matrix , int l1, int r1, int l2, int r2){
+        int sum = 0;
+        for(int i = l1; i <= l2; i++){
+            for(int j = r1; j <= r2 ; j++){
+                sum += matrix[i][j];
+            }
+        }
+        return sum;
+    }
+    static void findprefixsummatrix(int [][] matrix) {
+        int r = matrix.length;
+        int c = matrix[0].length;
+        // trverse horizontally to calculate ro wise prefix sum.
+        for (int i = 0; i < r; i++) {
+            for (int j = 1; j < c; j++) {
+                matrix[i][j] = matrix[i][j] + matrix[i][j - 1];
+            }
+        }
+
+        // travers vertically to calculate column wise sum
+        for (int j = 0; j < c; j++) {
+            for (int i = 1; i < r; i++) {
+                matrix[i][j] = matrix[i][j] + matrix[i - 1][j];
+            }
+        }
+    }
+    public static int findsum2(int [][] matrix , int l1, int r1, int l2, int r2){
+        int sum = 0;
+        findprefixsummatrix(matrix);
+        for(int i = l1; i <= l2; i++) {
+            if (r1 >= 1) {
+                sum += matrix[i][r2] - matrix[i][r1 - 1];
+            }
+            else{
+                sum += matrix[i][r2];
+            }
+        }
+        return sum;
+    }
+    public static int findsum3(int [][] matrix , int l1, int r1, int l2, int r2) {
+       int ans = 0;
+       int sum = 0 , up = 0, left = 0, leftup = 0;
+        findprefixsummatrix(matrix);
+        sum = matrix[l2][r2];
+        if(l1 >= 1) {
+            up = matrix[l1 - 1][r2];
+        }
+        if(r1 >= 1) {
+            left = matrix[l2][r1 - 1];
+        }
+        if(l1>=1 && r1 >= 1){
+            leftup = matrix[l1 - 1][r1 - 1];
+        }
+        ans = sum - up - left+ leftup;
+        return ans;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter np of rows and col : ");
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+        int [][] matrix = new int [r][c];
+        System.out.println("Enter" +r*c +" elements : ");
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("Enter recangle boundries l1, r1, l2, r2 ");
+        int l1 = sc.nextInt();
+        int r1 = sc.nextInt();
+        int l2 = sc.nextInt();
+        int r2 = sc.nextInt();
+        System.out.println("Rectangle sum method 1 => " + findsum(matrix,l1,r1,l1,r2));
+        System.out.println("Rectangle sum of method 3 => " + findsum3(matrix,l1,r1,l1,r2));
+
+    }
+}
+
+ */
 
 
 
