8c60b78dfa9ba59a07e986f88051ee3bfd67d700
rustamali-786 <rustamer786@gmail.com>
2025-10-01 01:00:06 +0530

    Add files via upload

---
+++ insertionsort.java
@@ -0,0 +1,19 @@
+/*
+package Recursion;
+
+public class insertionsort {
+    public static void main(String[] args) {
+        int [] arr = {4,1,17,31,9,2,8,6};
+        for (int i = 1; i < arr.length; i++) {
+            int j = i;
+            while (j > 0 && arr[j] < arr[j-1]){
+                int temp = arr[j];
+                arr[j] = arr[j-1];
+                arr[j-1] = temp;
+                j--;
+            }
+        }for(int ele: arr) System.out.println(ele);
+    }
+}
+
+ */
