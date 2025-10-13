2803aff5c4dbc3c624513da13cf86276de1ab818
rustamali-786 <rustamali2530683@gmail.com>
2025-10-13 20:10:17 +0530

    Add files via upload

--- BinarySearch.java
+++ BinarySearch.java
@@ -66,6 +66,76 @@ public class BinarySearch {
 Q. leetcode  find mountain in arry.
  */
 
+/*
+ Q. search element in sorted array.
+ */
+/*
+public class BinarySearch {
+    public static void main(String[] args) {
+        int [] nums = {4,5,6,7,0,1,2};
+        int target = 5;
+        System.out.println(searchRotated(nums,target));
+    }
+    public static int searchRotated(int [] nums,int target){
+        int n = nums.length;
+        int low = 0, high = n-1;
+        while(low<= high){
+            int mid = low+(high-low)/2;
+            if(nums[mid] == target) return mid;
+           else if(nums[low] < nums[mid]){
+                if(nums[low] <= target && target < nums[mid]) high = mid-1;
+                else low = mid+1;
+            }
+           else{
+               if(nums[mid] < target && target <= nums[high]) low = mid+1;
+               else high = mid-1;
+            }
+
+        }
+        return -1;
+    }
+}
+ */
+
+
+/*
+Q. search in sorted matrix
+ */
+/*
+public class BinarySearch {
+    public static void main(String[] args) {
+        int [][] matrix  = {{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,16}};
+        int target = 300;
+        System.out.println(sortmatrix(matrix,target));
+    }
+    public static boolean sortmatrix(int [][] matrix, int target) {
+        int row = matrix.length, col = matrix[0].length;
+        int low = 0, high = row * col - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int midRow = mid / col;
+            int midCol = mid % col;
+            if (matrix[midRow][midCol] == target) return true;
+            else if (matrix[midRow][midCol] < target) low = mid + 1;
+            else high = mid - 1;
+        }
+        return false;
+    }
+}
+
+ */
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
